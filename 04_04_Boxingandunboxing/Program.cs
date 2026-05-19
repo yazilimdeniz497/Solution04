570ae3de84a8d848684368eb6a693e56d6364410
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 11:11:37 +0300

    final 4.bölüm bitti

--- 04_04_Boxingandunboxing/Program.cs
+++ 04_04_Boxingandunboxing/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿object data = "Merhaba";
+if(data is string)
+{
+    Console.WriteLine("ifade string'tir");
+} else if(data is int)
+{
+    Console.WriteLine("ifade int'tir");
+}
+else
+{
+    Console.WriteLine("başka bir ifade");
+}
+Console.ReadKey();
