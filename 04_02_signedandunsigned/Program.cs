4b50f3450abd0a06658075996a2067d60f5b0c17
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-19 10:00:11 +0300

    2.proje eklendi

---
+++ 04_02_signedandunsigned/Program.cs
@@ -0,0 +1,16 @@
+﻿//byte
+Console.WriteLine("Byte Değer aralığı(0-255)");
+Console.WriteLine($"Byte minimum değeri:{byte.MinValue},Byte Maximum Değeri:{byte.MaxValue}");
+//sbyte
+Console.WriteLine("SByte Değer aralığı(-128 127)");
+Console.WriteLine($"Byte minimum değeri:{sbyte.MinValue},Byte Maximum Değeri:{sbyte.MaxValue}");
+Console.WriteLine(" ");
+byte unsignedvalue = 255;
+Console.WriteLine($"başlangıç değeri:{unsignedvalue}");
+unsignedvalue++;
+Console.WriteLine($"bitiş değeri:{unsignedvalue}");
+//ikili Gösterim
+Console.WriteLine("Binary Sistem");
+Console.WriteLine($"Bytin Binary karsiliği:{Convert.ToString(255,2)}");
+
+Console.ReadKey();
